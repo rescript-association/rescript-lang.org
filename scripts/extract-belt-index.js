4eb21df3f822e5abbb517244b56487a25cdb5f01
Patrick Stapfer <ryyppy@users.noreply.github.com>
2019-09-26 11:42:47 +0200

    Add Belt API index creation script with first unpolished index structure

---
+++ scripts/extract-belt-index.js
@@ -0,0 +1,85 @@
+/*
+ * This script is used for generating the search index of the Belt API
+ * Ideally we would like following information:
+ *
+ * - Module names (h1)
+ *   - function names (```re sig)
+ */
+const unified = require("unified");
+const markdown = require("remark-parse");
+const stringify = require("remark-stringify");
+const glob = require("glob");
+const path = require("path");
+const fs = require("fs");
+
+const codeblocks = require("./lib/codeblocks");
+
+const headers = options => (tree, file) => {
+  const headers = [];
+  let mainHeader;
+  tree.children.forEach(child => {
+    if (child.type === "heading" && child.depth === 1) {
+      if (child.children.length > 0) {
+        mainHeader = child.children[0].value;
+      }
+    }
+    if (child.type === "heading" && child.depth === 2) {
+      if (child.children.length > 0) {
+        headers.push(child.children[0].value);
+      }
+    }
+  });
+
+  file.data = Object.assign({}, file.data, { headers, mainHeader });
+};
+
+const filterBy = ({ meta, lang }) => {
+  return meta === "sig" && lang === "re";
+};
+
+// Codeblock formatter
+const formatter = value => {
+  // Strip newlines and weird spacing
+  return value
+    .replace(/\n/g, " ")
+    .replace(/\s+/g, " ")
+    .replace(/\(\s+/g, "(")
+    .replace(/\s+\)/g, ")");
+};
+
+const processor = unified()
+  .use(markdown, { gfm: true })
+  .use(stringify)
+  .use(headers)
+  .use(codeblocks, { filterBy, formatter });
+
+const toBeltDocsPath = filepath => {
+  return path.join("belt_docs", path.basename(filepath));
+};
+
+const BELT_MD_DIR = path.join(__dirname, "../pages/belt_docs");
+const INDEX_FILE = path.join(__dirname, "../index_data/belt_api_index.json");
+const files = glob.sync(`${BELT_MD_DIR}/*.md?(x)`);
+
+const processFile = filepath => {
+  const content = fs.readFileSync(filepath, "utf8");
+  const result = processor.processSync(content);
+
+  const filename = path.parse(filepath).name;
+
+  const dataset = {
+    headers: result.data.headers,
+    signatures: result.data.codeblocks.re,
+    href: path.join("belt_docs", filename),
+    moduleName: result.data.mainHeader,
+  };
+  return dataset;
+};
+
+const result = files.map(processFile);
+
+// TODO: Order data appropriately for the index
+const index = result;
+
+
+fs.writeFileSync(INDEX_FILE, JSON.stringify(index), 'utf8')
