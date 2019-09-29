500c105ccf22feb0fc22e6e24611d41b804b6882
Tomasz Cichociński <tomaszcichocinski@gmail.com>
2019-09-30 00:14:09 +0200

    chore(util): cleanup

--- common/Util.bs.js
+++ common/Util.bs.js
@@ -1,8 +1,5 @@
 
 
-import * as Belt_Id from "bs-platform/lib/es6/belt_Id.js";
-import * as Caml_obj from "bs-platform/lib/es6/caml_obj.js";
-import * as Belt_SetDict from "bs-platform/lib/es6/belt_SetDict.js";
 
 function s(prim) {
   return prim;
@@ -20,25 +17,8 @@ var ReactStuff = {
   Unsafe: Unsafe
 };
 
-var cmp = Caml_obj.caml_compare;
-
-var IntCmp = Belt_Id.MakeComparable({
-      cmp: cmp
-    });
-
-var b = Belt_SetDict.fromArray(/* array */[
-      1,
-      2,
-      3
-    ], IntCmp.cmp);
-
-var a = Belt_SetDict.empty;
-
 export {
   ReactStuff ,
-  IntCmp ,
-  a ,
-  b ,
   
 }
-/* IntCmp Not a pure module */
+/* No side effect */
--- common/Util.re
+++ common/Util.re
@@ -6,12 +6,3 @@ module ReactStuff = {
     external elementAsString: React.element => string = "%identity";
   };
 };
-
-module IntCmp =
-  Belt.Id.MakeComparable({
-    type t = int;
-    let cmp = Pervasives.compare;
-  });
-
-let a = Belt.Set.Dict.empty;
-let b = Belt.Set.Dict.fromArray([|1, 2, 3|], ~cmp=IntCmp.cmp);
