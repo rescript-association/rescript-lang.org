a6a7423c0948c867f58226a27aa3d9bfc2c191a9
Nik Graf <nik@nikgraf.com>
2019-10-27 23:00:24 +0100

    add JS docs layouts

---
+++ layouts/JsDocsLayout.bs.js
@@ -0,0 +1,386 @@
+
+
+import * as $$Text from "../components/Text.bs.js";
+import * as Util from "../common/Util.bs.js";
+import * as Curry from "bs-platform/lib/es6/curry.js";
+import * as React from "react";
+import * as Js_dict from "bs-platform/lib/es6/js_dict.js";
+import * as Link from "next/link";
+import * as Belt_Array from "bs-platform/lib/es6/belt_Array.js";
+import * as Belt_Option from "bs-platform/lib/es6/belt_Option.js";
+import * as Caml_option from "bs-platform/lib/es6/caml_option.js";
+import * as Router from "next/router";
+import * as React$1 from "@mdx-js/react";
+import * as Caml_chrome_debugger from "bs-platform/lib/es6/caml_chrome_debugger.js";
+
+require('../styles/main.css')
+;
+
+
+let hljs = require('highlight.js/lib/highlight');
+let reasonHighlightJs = require('reason-highlightjs');
+hljs.registerLanguage('reason', reasonHighlightJs);
+
+;
+
+var indexData = (require('../index_data/js_api_index.json'));
+
+function JsDocsLayout$Md$Anchor(Props) {
+  var id = Props.id;
+  var style = {
+    position: "absolute",
+    top: "-7rem"
+  };
+  return React.createElement("span", {
+              className: "relative"
+            }, React.createElement("a", {
+                  className: "mr-2 text-main-lighten-65 hover:cursor-pointer",
+                  href: "#" + id
+                }, Util.ReactStuff.s("#")), React.createElement("a", {
+                  id: id,
+                  style: style
+                }));
+}
+
+var Anchor = {
+  make: JsDocsLayout$Md$Anchor
+};
+
+function JsDocsLayout$Md$InvisibleAnchor(Props) {
+  var id = Props.id;
+  var style = {
+    position: "absolute",
+    top: "-1rem"
+  };
+  return React.createElement("span", {
+              "aria-hidden": true,
+              className: "relative"
+            }, React.createElement("a", {
+                  id: id,
+                  style: style
+                }));
+}
+
+var InvisibleAnchor = {
+  make: JsDocsLayout$Md$InvisibleAnchor
+};
+
+function JsDocsLayout$Md$H2(Props) {
+  var children = Props.children;
+  return React.createElement(React.Fragment, undefined, React.createElement(JsDocsLayout$Md$InvisibleAnchor, {
+                  id: children
+                }), React.createElement("div", {
+                  className: "border-b border-gray-200 my-20"
+                }));
+}
+
+var H2 = {
+  make: JsDocsLayout$Md$H2
+};
+
+function JsDocsLayout$Md$Pre(Props) {
+  var children = Props.children;
+  return React.createElement("pre", {
+              className: "mt-2 mb-4 block"
+            }, children);
+}
+
+var Pre = {
+  make: JsDocsLayout$Md$Pre
+};
+
+function JsDocsLayout$Md$P(Props) {
+  var children = Props.children;
+  return React.createElement("p", {
+              className: "mt-3 leading-4 text-main-lighten-15"
+            }, children);
+}
+
+var P = {
+  make: JsDocsLayout$Md$P
+};
+
+var components = {
+  p: JsDocsLayout$Md$P,
+  li: $$Text.Md.Li.make,
+  h1: $$Text.H1.make,
+  h2: JsDocsLayout$Md$H2,
+  h3: $$Text.H3.make,
+  h4: $$Text.H4.make,
+  h5: $$Text.H5.make,
+  ul: $$Text.Md.Ul.make,
+  ol: $$Text.Md.Ol.make,
+  inlineCode: $$Text.Md.InlineCode.make,
+  code: $$Text.Md.Code.make,
+  pre: JsDocsLayout$Md$Pre,
+  a: $$Text.Md.A.make
+};
+
+var Md = {
+  Anchor: Anchor,
+  InvisibleAnchor: InvisibleAnchor,
+  H2: H2,
+  Pre: Pre,
+  P: P,
+  components: components
+};
+
+var $$package = (require('../package.json'));
+
+var link = "no-underline text-inherit hover:text-white";
+
+function JsDocsLayout$Navigation(Props) {
+  return React.createElement("nav", {
+              className: "fixed z-10 top-0 p-2 w-full h-16 shadow flex items-center text-ghost-white text-sm bg-bs-purple",
+              id: "header"
+            }, React.createElement(Link.default, {
+                  href: "/js_docs",
+                  children: React.createElement("a", {
+                        className: "flex items-center pl-10 w-1/5"
+                      }, React.createElement("div", {
+                            className: "h-6 w-6 bg-white rounded-full flex flex-col justify-center items-center"
+                          }, React.createElement("div", {
+                                className: "h-4 w-4 bg-bs-purple rounded-full"
+                              })), React.createElement("span", {
+                            className: "text-xl ml-2 font-black text-white"
+                          }, Util.ReactStuff.s("Js")))
+                }), React.createElement("div", {
+                  className: "ml-6 flex w-3/5 px-3 h-10 max-w-sm rounded-lg text-white bg-light-grey-20 content-center items-center w-2/3"
+                }, React.createElement("img", {
+                      "aria-hidden": true,
+                      className: "mr-3",
+                      src: "/static/ic_search_small.svg"
+                    }), React.createElement("input", {
+                      className: "bg-transparent placeholder-ghost-white block focus:outline-none w-full ml-2",
+                      placeholder: "Search not ready yet...",
+                      type: "text"
+                    })), React.createElement("div", {
+                  className: "flex mx-4 text-ghost-white justify-between ml-auto"
+                }, React.createElement(Link.default, {
+                      href: "/",
+                      children: React.createElement("a", {
+                            className: link
+                          }, Util.ReactStuff.s("ReasonML"))
+                    }), React.createElement("a", {
+                      className: "no-underline text-inherit hover:text-white align-middle ml-6",
+                      href: "https://github.com/reason-association/reasonml.org",
+                      rel: "noopener noreferrer",
+                      target: "_blank"
+                    }, Util.ReactStuff.s("Github")), React.createElement("a", {
+                      className: "bg-light-grey-20 leading-normal ml-6 px-1 rounded text-light-grey text-xs",
+                      href: "https://github.com/BuckleScript/bucklescript/releases",
+                      rel: "noopener noreferrer",
+                      target: "_blank"
+                    }, Util.ReactStuff.s("v" + $$package.dependencies["bs-platform"]))));
+}
+
+var Navigation = {
+  link: link,
+  make: JsDocsLayout$Navigation
+};
+
+var overviewNavs = /* array */[/* record */Caml_chrome_debugger.record([
+      "name",
+      "href"
+    ], [
+      "Introduction",
+      "/js_docs"
+    ])];
+
+var apiNavs = /* array */[/* record */Caml_chrome_debugger.record([
+      "name",
+      "href"
+    ], [
+      "Array2",
+      "/js_docs/array-2"
+    ])];
+
+var categories = /* array */[
+  /* record */Caml_chrome_debugger.record([
+      "name",
+      "items"
+    ], [
+      "Overview",
+      overviewNavs
+    ]),
+  /* record */Caml_chrome_debugger.record([
+      "name",
+      "items"
+    ], [
+      "API",
+      apiNavs
+    ])
+];
+
+function JsDocsLayout$Sidebar$NavUl(Props) {
+  var match = Props.isItemActive;
+  var isItemActive = match !== undefined ? match : (function (_nav) {
+        return false;
+      });
+  var match$1 = Props.isHidden;
+  var isHidden = match$1 !== undefined ? match$1 : false;
+  var items = Props.items;
+  return React.createElement("ul", {
+              className: "ml-2 mt-1 text-main-lighten-15"
+            }, Util.ReactStuff.ate(Belt_Array.map(items, (function (m) {
+                        var hidden = isHidden ? "hidden" : "block";
+                        var match = Curry._1(isItemActive, m);
+                        var active = match ? " bg-bs-purple-lighten-95 text-bs-pink rounded -ml-1 px-2 font-bold block " : "";
+                        return React.createElement("li", {
+                                    key: m[/* name */0],
+                                    className: hidden + " leading-5 w-4/5",
+                                    tabIndex: 0
+                                  }, React.createElement("a", {
+                                        className: "hover:text-bs-purple " + active,
+                                        href: m[/* href */1]
+                                      }, Util.ReactStuff.s(m[/* name */0])));
+                      }))));
+}
+
+var NavUl = {
+  make: JsDocsLayout$Sidebar$NavUl
+};
+
+function categoryToElement(isItemActive, category) {
+  var tmp = {
+    items: category[/* items */1]
+  };
+  if (isItemActive !== undefined) {
+    tmp.isItemActive = Caml_option.valFromOption(isItemActive);
+  }
+  return React.createElement("div", {
+              key: category[/* name */0],
+              className: "my-12"
+            }, React.createElement($$Text.Overline.make, {
+                  children: Util.ReactStuff.s(category[/* name */0])
+                }), React.createElement(JsDocsLayout$Sidebar$NavUl, tmp));
+}
+
+function JsDocsLayout$Sidebar$ModuleContent(Props) {
+  var isItemActive = Props.isItemActive;
+  var headers = Props.headers;
+  var moduleName = Props.moduleName;
+  var match = React.useState((function () {
+          return false;
+        }));
+  var setCollapsed = match[1];
+  var collapsed = match[0];
+  var items = Belt_Array.map(headers, (function (header) {
+          return /* record */Caml_chrome_debugger.record([
+                    "name",
+                    "href"
+                  ], [
+                    header,
+                    "#" + header
+                  ]);
+        }));
+  var tmp = {
+    isHidden: collapsed,
+    items: items
+  };
+  if (isItemActive !== undefined) {
+    tmp.isItemActive = Caml_option.valFromOption(isItemActive);
+  }
+  return React.createElement("div", {
+              className: "my-12"
+            }, React.createElement($$Text.Overline.make, {
+                  children: React.createElement("a", {
+                        className: "cursor-pointer hover:text-bs-purple",
+                        href: "#",
+                        onClick: (function (evt) {
+                            evt.preventDefault();
+                            return Curry._1(setCollapsed, (function (isCollapsed) {
+                                          return !isCollapsed;
+                                        }));
+                          })
+                      }, React.createElement("span", {
+                            className: "hidden hover:block"
+                          }, Util.ReactStuff.s(collapsed ? "v" : "^")), Util.ReactStuff.s(moduleName))
+                }), React.createElement(JsDocsLayout$Sidebar$NavUl, tmp));
+}
+
+var ModuleContent = {
+  make: JsDocsLayout$Sidebar$ModuleContent
+};
+
+function JsDocsLayout$Sidebar(Props) {
+  var route = Props.route;
+  var headers = Belt_Option.getWithDefault(Belt_Option.map(Js_dict.get(indexData, route), (function (data) {
+              return data.headers;
+            })), /* array */[]);
+  var moduleName = Belt_Option.getWithDefault(Belt_Option.map(Js_dict.get(indexData, route), (function (data) {
+              return data.moduleName;
+            })), "?");
+  var isItemActive = function (navItem) {
+    return navItem[/* href */1] === route;
+  };
+  var match = route !== "/js_docs";
+  var partial_arg = isItemActive;
+  return React.createElement("div", {
+              className: "pl-2 flex w-full justify-center h-auto overflow-y-visible block bg-light-grey",
+              style: {
+                maxWidth: "17.5rem"
+              }
+            }, React.createElement("nav", {
+                  className: "relative w-48 sticky h-screen block overflow-y-auto scrolling-touch pb-32",
+                  style: {
+                    top: "4rem"
+                  }
+                }, match ? React.createElement(JsDocsLayout$Sidebar$ModuleContent, {
+                        headers: headers,
+                        moduleName: moduleName
+                      }) : null, React.createElement("div", undefined, Util.ReactStuff.ate(Belt_Array.map(categories, (function (param) {
+                                return categoryToElement(partial_arg, param);
+                              }))))));
+}
+
+var Sidebar = {
+  overviewNavs: overviewNavs,
+  apiNavs: apiNavs,
+  categories: categories,
+  NavUl: NavUl,
+  categoryToElement: categoryToElement,
+  ModuleContent: ModuleContent,
+  make: JsDocsLayout$Sidebar
+};
+
+function JsDocsLayout(Props) {
+  var match = Props.components;
+  var components$1 = match !== undefined ? Caml_option.valFromOption(match) : components;
+  var children = Props.children;
+  var router = Router.useRouter();
+  var minWidth = {
+    minWidth: "20rem"
+  };
+  return React.createElement("div", undefined, React.createElement("div", {
+                  className: "max-w-4xl w-full",
+                  style: minWidth
+                }, React.createElement(JsDocsLayout$Navigation, { }), React.createElement("div", {
+                      className: "flex mt-12"
+                    }, React.createElement(JsDocsLayout$Sidebar, {
+                          route: router.route
+                        }), React.createElement("main", {
+                          className: "pt-12 w-4/5 static min-h-screen overflow-visible"
+                        }, React.createElement(React$1.MDXProvider, {
+                              components: components$1,
+                              children: React.createElement("div", {
+                                    className: "pl-8 max-w-md mb-32 text-lg"
+                                  }, children)
+                            })))));
+}
+
+var Link$1 = 0;
+
+var make = JsDocsLayout;
+
+export {
+  Link$1 as Link,
+  indexData ,
+  Md ,
+  $$package ,
+  Navigation ,
+  Sidebar ,
+  make ,
+  
+}
+/*  Not a pure module */
---
+++ layouts/JsDocsLayout.re
@@ -0,0 +1,317 @@
+%raw
+"require('../styles/main.css')";
+
+%raw
+{|
+let hljs = require('highlight.js/lib/highlight');
+let reasonHighlightJs = require('reason-highlightjs');
+hljs.registerLanguage('reason', reasonHighlightJs);
+|};
+
+open Util.ReactStuff;
+open Text;
+module Link = Next.Link;
+
+// Structure defined by `scripts/extract-belt-index.js`
+let indexData:
+  Js.Dict.t({
+    .
+    "signatures": array(string),
+    "moduleName": string,
+    "headers": array(string),
+  }) = [%raw
+  "require('../index_data/js_api_index.json')"
+];
+
+/*
+    We use some custom markdown styling for the Js docs to make
+    it easier on the eyes
+ */
+module Md = {
+  module Anchor = {
+    [@react.component]
+    let make = (~id: string) => {
+      let style =
+        ReactDOMRe.Style.make(~position="absolute", ~top="-7rem", ());
+      <span className="relative">
+        <a
+          className="mr-2 text-main-lighten-65 hover:cursor-pointer"
+          href={"#" ++ id}>
+          {j|#|j}->s
+        </a>
+        <a style id />
+      </span>;
+    };
+  };
+
+  module InvisibleAnchor = {
+    [@react.component]
+    let make = (~id: string) => {
+      let style =
+        ReactDOMRe.Style.make(~position="absolute", ~top="-1rem", ());
+      <span className="relative" ariaHidden=true> <a id style /> </span>;
+    };
+  };
+
+  module H2 = {
+    // We will currently hide the headline, to keep the structure,
+    // but having an Elm like documentation
+    [@react.component]
+    let make = (~children) => {
+      <>
+        // Here we know that children is always a string (## headline)
+        <InvisibleAnchor id={children->Unsafe.elementAsString} />
+        <div className="border-b border-gray-200 my-20" />
+        /*
+         <h2
+           className="inline text-xl leading-3 font-montserrat font-medium text-main-black">
+           <Anchor id={children->Unsafe.elementAsString} />
+         </h2>
+         */
+      </>;
+    };
+  };
+
+  module Pre = {
+    [@react.component]
+    let make = (~children) => {
+      <pre className="mt-2 mb-4 block"> children </pre>;
+    };
+  };
+
+  module P = {
+    [@react.component]
+    let make = (~children) => {
+      <p className="mt-3 leading-4 text-main-lighten-15"> children </p>;
+    };
+  };
+
+  let components =
+    Mdx.Components.t(
+      ~p=P.make,
+      ~li=Md.Li.make,
+      ~h1=H1.make,
+      ~h2=H2.make,
+      ~h3=H3.make,
+      ~h4=H4.make,
+      ~h5=H5.make,
+      ~ul=Md.Ul.make,
+      ~ol=Md.Ol.make,
+      ~a=Md.A.make,
+      ~pre=Pre.make,
+      ~inlineCode=Md.InlineCode.make,
+      ~code=Md.Code.make,
+      (),
+    );
+};
+
+// Retrieve package.json to access the version of bs-platform.
+let package: {. "dependencies": {. "bs-platform": string}} = [%raw
+  "require('../package.json')"
+];
+
+module Navigation = {
+  let link = "no-underline text-inherit hover:text-white";
+  [@react.component]
+  let make = () =>
+    <nav
+      id="header"
+      className="fixed z-10 top-0 p-2 w-full h-16 shadow flex items-center text-ghost-white text-sm bg-bs-purple">
+      <Link href="/js_docs">
+        <a className="flex items-center pl-10 w-1/5">
+          <div
+            className="h-6 w-6 bg-white rounded-full flex flex-col justify-center items-center">
+            <div className="h-4 w-4 bg-bs-purple rounded-full" />
+          </div>
+          <span className="text-xl ml-2 font-black text-white"> "Js"->s </span>
+        </a>
+      </Link>
+      <div
+        className="ml-6 flex w-3/5 px-3 h-10 max-w-sm rounded-lg text-white bg-light-grey-20 content-center items-center w-2/3">
+        <img
+          src="/static/ic_search_small.svg"
+          className="mr-3"
+          ariaHidden=true
+        />
+        <input
+          type_="text"
+          className="bg-transparent placeholder-ghost-white block focus:outline-none w-full ml-2"
+          placeholder="Search not ready yet..."
+        />
+      </div>
+      <div className="flex mx-4 text-ghost-white justify-between ml-auto">
+        <Link href="/"> <a className=link> "ReasonML"->s </a> </Link>
+        <a
+          href="https://github.com/reason-association/reasonml.org"
+          rel="noopener noreferrer"
+          target="_blank"
+          className={link ++ " align-middle ml-6"}>
+          "Github"->s
+        </a>
+        <a
+          href="https://github.com/BuckleScript/bucklescript/releases"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="bg-light-grey-20 leading-normal ml-6 px-1 rounded text-light-grey text-xs">
+          {("v" ++ package##dependencies##"bs-platform")->s}
+        </a>
+      </div>
+    </nav>;
+};
+
+module Sidebar = {
+  // Navigation point information
+  type navItem = {
+    name: string,
+    href: string,
+  };
+
+  type category = {
+    name: string,
+    items: array(navItem),
+  };
+
+  let overviewNavs = [|{name: "Introduction", href: "/js_docs"}|];
+
+  let apiNavs = [|{name: "Array2", href: "/js_docs/array-2"}|];
+
+  let categories = [|
+    {name: "Overview", items: overviewNavs},
+    {name: "API", items: apiNavs},
+  |];
+
+  module NavUl = {
+    [@react.component]
+    let make =
+        (
+          ~isItemActive: navItem => bool=_nav => false,
+          ~isHidden=false,
+          ~items: array(navItem),
+        ) => {
+      <ul className="ml-2 mt-1 text-main-lighten-15">
+        {Belt.Array.map(
+           items,
+           m => {
+             let hidden = isHidden ? "hidden" : "block";
+             let active =
+               isItemActive(m)
+                 ? " bg-bs-purple-lighten-95 text-bs-pink rounded -ml-1 px-2 font-bold block "
+                 : "";
+             <li
+               key={m.name}
+               className={hidden ++ " leading-5 w-4/5"}
+               // to make non-interactive elements (like div, span or li) tab-able
+               // see https://developer.mozilla.org/en-US/docs/Web/Accessibility/Keyboard-navigable_JavaScript_widgets
+               tabIndex=0>
+               <a href={m.href} className={"hover:text-bs-purple " ++ active}>
+                 m.name->s
+               </a>
+             </li>;
+           },
+         )
+         ->ate}
+      </ul>;
+    };
+  };
+
+  let categoryToElement =
+      (~isItemActive: option(navItem => bool)=?, category: category)
+      : React.element => {
+    <div key={category.name} className="my-12">
+      <Overline> category.name->s </Overline>
+      <NavUl ?isItemActive items={category.items} />
+    </div>;
+  };
+
+  module ModuleContent = {
+    [@react.component]
+    let make =
+        (~isItemActive=?, ~headers: array(string), ~moduleName: string) => {
+      let (collapsed, setCollapsed) = React.useState(() => false);
+      let items =
+        Belt.Array.map(headers, header =>
+          {name: header, href: "#" ++ header}
+        );
+      <div className="my-12">
+        <Overline>
+          <a
+            className="cursor-pointer hover:text-bs-purple"
+            href="#"
+            onClick={evt => {
+              ReactEvent.Mouse.preventDefault(evt);
+              setCollapsed(isCollapsed => !isCollapsed);
+            }}>
+            <span className="hidden hover:block">
+              {collapsed ? "v" : "^"}->s
+            </span>
+            moduleName->s
+          </a>
+        </Overline>
+        <NavUl ?isItemActive items isHidden=collapsed />
+      </div>;
+    };
+  };
+
+  // subitems: list of functions inside given module (defined by route)
+  [@react.component]
+  let make = (~route: string) => {
+    let headers =
+      Belt.Option.(
+        Js.Dict.get(indexData, route)
+        ->map(data => data##headers)
+        ->getWithDefault([||])
+      );
+
+    let moduleName =
+      Belt.Option.(
+        Js.Dict.get(indexData, route)
+        ->map(data => data##moduleName)
+        ->getWithDefault("?")
+      );
+
+    let isItemActive = navItem => {
+      navItem.href === route;
+    };
+
+    <div
+      className="pl-2 flex w-full justify-center h-auto overflow-y-visible block bg-light-grey"
+      style={Style.make(~maxWidth="17.5rem", ())}>
+      <nav
+        className="relative w-48 sticky h-screen block overflow-y-auto scrolling-touch pb-32"
+        style={Style.make(~top="4rem", ())}>
+        {route !== "/js_docs"
+           ? <ModuleContent
+               headers
+               // Todo: We need to introduce router state to be able to
+               //       listen to anchor changes (#get, #map,...)
+               moduleName
+               //       Add a `isItemActive` as soon as we have access to this info
+             />
+           : React.null}
+        <div>
+          {categories->Belt.Array.map(categoryToElement(~isItemActive))->ate}
+        </div>
+      </nav>
+    </div>;
+  };
+};
+
+[@react.component]
+let make = (~components=Md.components, ~children) => {
+  let router = Next.Router.useRouter();
+
+  let minWidth = ReactDOMRe.Style.make(~minWidth="20rem", ());
+  <div>
+    <div className="max-w-4xl w-full" style=minWidth>
+      <Navigation />
+      <div className="flex mt-12">
+        <Sidebar route={router##route} />
+        <main className="pt-12 w-4/5 static min-h-screen overflow-visible">
+          <Mdx.Provider components>
+            <div className="pl-8 max-w-md mb-32 text-lg"> children </div>
+          </Mdx.Provider>
+        </main>
+      </div>
+    </div>
+  </div>;
+};
