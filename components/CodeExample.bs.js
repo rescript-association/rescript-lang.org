aa644fe0ad81c7e830c8aaf99786df9bbbfd598b
Patrick Stapfer <ryyppy@users.noreply.github.com>
2020-02-05 13:10:55 +0100

    Get rid of last montserrat occurrence in CodeExample component

--- components/CodeExample.bs.js
+++ components/CodeExample.bs.js
@@ -32,7 +32,7 @@ function CodeExample(Props) {
   return React.createElement("div", {
               className: "flex flex-col -mx-8 xs:mx-0 rounded-none xs:rounded border border-snow-dark bg-snow-light py-2 px-3 mt-10 text-night-dark"
             }, React.createElement("div", {
-                  className: "font-montserrat text-sm mb-3 font-bold text-fire"
+                  className: "font-sans text-sm mb-3 font-bold text-fire"
                 }, Util.ReactStuff.s(langShortname.toUpperCase())), React.createElement("div", {
                   className: "pl-5 text-base pb-4 overflow-x-auto"
                 }, children));
--- components/CodeExample.re
+++ components/CodeExample.re
@@ -27,7 +27,7 @@ let make = (~code: string, ~lang) => {
 
   <div
     className="flex flex-col -mx-8 xs:mx-0 rounded-none xs:rounded border border-snow-dark bg-snow-light py-2 px-3 mt-10 text-night-dark">
-    <div className="font-montserrat text-sm mb-3 font-bold text-fire">
+    <div className="font-sans text-sm mb-3 font-bold text-fire">
       {Js.String2.toUpperCase(langShortname)->s}
     </div>
     <div className="pl-5 text-base pb-4 overflow-x-auto"> children </div>
