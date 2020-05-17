86145d87c1fd74409a4dd0e5f392bb25e8c7cb41
Patrick Stapfer <ryyppy@users.noreply.github.com>
2020-05-17 13:01:17 +0200

    Add initial _document with simpleanalytics script

---
+++ pages/_document.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class extends Document {
+  render() {
+    const isProduction = process.env.ENV === "production";
+    return (
+      <html>
+        <Head></Head>
+        <body>
+          <Main />
+          <NextScript />
+
+          {isProduction && (
+            <Fragment>
+              <script
+                async
+                defer
+                src="https://scripts.simpleanalyticscdn.com/latest.js"
+              />
+              <noscript>
+                <img
+                  src="https://queue.simpleanalyticscdn.com/noscript.gif"
+                  alt=""
+                />
+              </noscript>
+            </Fragment>
+          )}
+        </body>
+      </html>
+    );
+  }
+}
