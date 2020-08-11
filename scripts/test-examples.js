d04bad14566836300848906609081c85abcd93b8
Cheng Lou <chenglou92@gmail.com>
2020-08-11 09:48:31 -0700

    Add new test-examples script
    The old script was too slow, complex and error-prone. I've simplified it while making it faster and more robust.
    
    The insight is that the mdx files can just be treated as an alternative syntax for ReScript, where each prose line is a discardable comment and each ```res block is the actual code.
    
    We can then write a minimal and verifiably correct mini parser to turn a mdx file into a legit ReScript syntax file, then compile using `bsc -i`.
    
    `` ```res example`` becomes a module block
    `` ```res sig`` becomes a module type block
    `` ```res prelude`` becomes naked statements
    
    This provides a much more familiar mental model while writing the doc pages. And the semantics of the prelude block work out without needing out-of-band knowledge of test-examples's specific interpretation of it.
    
    I've taken the occasion to clean up the code:
    - Only deps are glob and node stdlib (faster startup)
    - mdx-agnostic, lol!
    - 250L -> 70L
    - Robust and fast error regex replacement
    - No more promises and async/await (faster, cleaner code)
    - Fewer invocations of bsc (way faster)
    
    End-developer ergonomics improvements:
    - Script running went from 3.6s to 0.18s (20x improvement)
    - Idiomatic, familiar error output instead of a custom one
    - Script's now fast enough to simplify the rest of the pipeline. Can be run after any edit operation (haven't changed the pipeline in this diff. Didn't bother)
    - Because the script's fast now, no more need to manually incrementally run a single file's checks. Just rerun everything.

---
+++ scripts/test-examples.js
@@ -0,0 +1,68 @@
+const glob = require("glob");
+const fs = require("fs");
+const child_process = require("child_process");
+const path = require("path");
+
+let tempFileName = path.join(__dirname, '..', '_tempFile.res')
+let tempFileNameRegex = /_tempFile\.res/g
+let bsc = path.join(__dirname, '..', 'node_modules', 'bs-platform', process.platform, 'bsc.exe')
+
+let parseFile = content => {
+  if (!/```res (example|prelude|sig)/.test(content)) {
+    return
+  }
+
+  let inCodeBlock = false
+  let moduleId = 0
+  return content.split('\n').map(line => {
+    let modifiedLine = ''
+    if (line.startsWith('```res example')) {
+      inCodeBlock = true
+      modifiedLine = `/* _MODULE_EXAMPLE_START */ module M_${moduleId++} = {`
+    } else if (line.startsWith('```res prelude')) {
+      inCodeBlock = true
+      modifiedLine = `/* _MODULE_PRELUDE_START */ include {`
+    } else if (line.startsWith('```res sig')) {
+      inCodeBlock = true
+      modifiedLine = `/* _MODULE_SIG_START */ module type M_${moduleId++} = {`
+    } else if (inCodeBlock) {
+      if (line.startsWith('```')) {
+        inCodeBlock = false
+        modifiedLine = '} // _MODULE_END'
+      } else {
+        modifiedLine = line
+      }
+    }
+
+    return modifiedLine
+  }).join('\n')
+}
+
+let postprocessOutput = (file, error) => {
+  return error.stderr.toString()
+    .replace(tempFileNameRegex, path.relative('.', file))
+    .replace(/\/\* _MODULE_(EXAMPLE|PRELUDE|SIG)_START \*\/.+/g, (_, capture) => {
+      return '```res ' + (capture === 'EXAMPLE' ? 'example' : capture === 'PRELUDE' ? 'prelude' : 'sig')
+    })
+    .replace(/(.*)\}(.*)\/\/ _MODULE_END/g, (_, cap1, cap2) => {
+      // cap1 cap2 might be empty or ansi coloring code
+      return cap1 + '```' + cap2
+    })
+}
+
+fs.writeFileSync(tempFileName, '')
+
+glob.sync(__dirname + '/../pages/**/*.mdx').forEach((file) => {
+  let content = fs.readFileSync(file, {encoding: 'utf-8'})
+  let parsedResult = parseFile(content)
+  if (parsedResult != null) {
+    fs.writeFileSync(tempFileName, parsedResult)
+    try {
+      child_process.execFileSync(bsc, ['-i', tempFileName], {stdio: 'pipe'})
+    } catch (e) {
+      process.stdout.write(postprocessOutput(file, e))
+    }
+  }
+})
+
+fs.unlinkSync(tempFileName)
