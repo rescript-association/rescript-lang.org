5c2ff51b353352ce895db57a5af82d0b77da1826
Cheng Lou <chenglou92@gmail.com>
2020-08-11 12:23:59 -0700

    Revamp test-examples.js (#15)
    * Remove old test-examples script
    
    * Add new test-examples script
    
    The old script was too slow, complex and error-prone. I've simplified it while making it faster and more robust.
    
    The insight is that the mdx files can just be treated as an alternative syntax for ReScript, where each prose line is a discardable comment and each ```res block is the actual code.
    
    We can then write a minimal and verifiably correct mini parser to turn a mdx file into a legit ReScript syntax file, then compile using `bsc -i`.
    
    `` ```res example`` becomes a module block
    `` ```res sig`` becomes a module type block
    `` ```res prelude`` becomes naked statements
    
    This provides a much more familiar mental model while writing the doc pages. And the semantics of the prelude block work out without needing out-of-band knowledge of test-examples's specific interpretation of it.
    
    I've taken the occasion to clean up the code:
    - Only deps are glob and node stdlib (faster startup)
    - mdx-agnostic, lol!
    - 250L -> 70L
    - Robust and fast error regex replacement
    - No more promises and async/await (faster, cleaner code)
    - Fewer invocations of bsc (way faster)
    
    End-developer ergonomics improvements:
    - Script running went from 3.6s to 0.18s (20x improvement)
    - Idiomatic, familiar error output instead of a custom one
    - Script's now fast enough to simplify the rest of the pipeline. Can be run after any edit operation (haven't changed the pipeline in this diff. Didn't bother)
    - Because the script's fast now, no more need to manually incrementally run a single file's checks. Just rerun everything.

--- scripts/test-examples.js
+++ scripts/test-examples.js
@@ -1,244 +1,68 @@
-/*
- * This script globs all `pages/*.mdx` files and
- * parses all codeblocks with an `example` and `prelude` meta string.
- *
- * For every mdx files with prelude, it will prepand all prelude snippets on each
- * parse example.
- *
- * It will then run the full prelude + example code through `bsc -i -e
- * '[code]'` (only since version 5.2) and check if there was a non-0 exit code.
- *
- * If an error was found, then stderr will be modified to output the right
- * error location in the mdx file (pointing to the correct line inside the
- * codeblock)
- *
- * This script can also be run for a subset of files via a glob:
- *
- * ```
- * # Don't forget the doubleticks, otherwise asterisks might not be recognized
- * node scripts/test-examples.js "pages/belt_docs/array.mdx"
- * ```
- *
- */
-const unified = require("unified");
-const markdown = require("remark-parse");
-const stringify = require("remark-stringify");
 const glob = require("glob");
-const path = require("path");
 const fs = require("fs");
 const child_process = require("child_process");
-const util = require("util");
-const tmp = require("tmp");
+const path = require("path");
 
-const execFile = util.promisify(child_process.execFile);
+let tempFileName = path.join(__dirname, '..', '_tempFile.res')
+let tempFileNameRegex = /_tempFile\.res/g
+let bsc = path.join(__dirname, '..', 'node_modules', 'bs-platform', process.platform, 'bsc.exe')
 
-// Plugin for parsing the test candiates
-const candidates = options => (tree, file) => {
-  const signatures = [];
-  const examples = [];
-  const preludes = [];
+let parseFile = content => {
+  if (!/```res (example|prelude|sig)/.test(content)) {
+    return
+  }
 
-  const formatter = v => v;
-
-  const { children } = tree;
-
-  children.forEach(child => {
-    if (child.type === "code" && child.value) {
-      const { meta, lang} = child;
-      if (lang === "res") {
-        if (meta === "sig") {
-          signatures.push(formatter(child.value));
-        } else if (meta === "example") {
-          const line = child.position.start.line;
-          examples.push({ value: formatter(child.value), line });
-        } else if (meta === "prelude") {
-          const line = child.position.start.line;
-          preludes.push({ value: formatter(child.value), line });
-        }
+  let inCodeBlock = false
+  let moduleId = 0
+  return content.split('\n').map(line => {
+    let modifiedLine = ''
+    if (line.startsWith('```res example')) {
+      inCodeBlock = true
+      modifiedLine = `/* _MODULE_EXAMPLE_START */ module M_${moduleId++} = {`
+    } else if (line.startsWith('```res prelude')) {
+      inCodeBlock = true
+      modifiedLine = `/* _MODULE_PRELUDE_START */ include {`
+    } else if (line.startsWith('```res sig')) {
+      inCodeBlock = true
+      modifiedLine = `/* _MODULE_SIG_START */ module type M_${moduleId++} = {`
+    } else if (inCodeBlock) {
+      if (line.startsWith('```')) {
+        inCodeBlock = false
+        modifiedLine = '} // _MODULE_END'
+      } else {
+        modifiedLine = line
       }
     }
-  });
 
-  const candidates = { signatures, examples, preludes };
-  file.data = Object.assign({}, file.data, { candidates });
-};
+    return modifiedLine
+  }).join('\n')
+}
 
-const processor = unified()
-  .use(markdown, { gfm: true })
-  .use(stringify)
-  .use(candidates);
+let postprocessOutput = (file, error) => {
+  return error.stderr.toString()
+    .replace(tempFileNameRegex, path.relative('.', file))
+    .replace(/\/\* _MODULE_(EXAMPLE|PRELUDE|SIG)_START \*\/.+/g, (_, capture) => {
+      return '```res ' + (capture === 'EXAMPLE' ? 'example' : capture === 'PRELUDE' ? 'prelude' : 'sig')
+    })
+    .replace(/(.*)\}(.*)\/\/ _MODULE_END/g, (_, cap1, cap2) => {
+      // cap1 cap2 might be empty or ansi coloring code
+      return cap1 + '```' + cap2
+    })
+}
 
-const MD_DIR = path.join(__dirname, "../pages");
+fs.writeFileSync(tempFileName, '')
 
-const BSC = path.join(__dirname, "../node_modules/.bin/bsc");
-
-// relFilepath: instead of whole absolute path, just paths like `pages/...`
-// prelude: All prelude snippets in one string which will be prepended to each example
-const testExample = async (relFilepath, example, prelude) => {
-  const { line, value } = example;
-
-  const preludeLines = prelude !== "" ? prelude.split("\n").length : 0;
-
-  const tempFile = tmp.fileSync();
-  const fileToFormat = tempFile.name + '.res';
-  const fullCode = prelude ? `${prelude}\n${value}` : value;
-  fs.writeFileSync(fileToFormat, fullCode);
-
-  console.log(`Testing example in '${relFilepath}' on line ${line}...`);
-  try {
-    const ret = await execFile(BSC, ["-i", fileToFormat]);
-    return { status: "ok" };
-  } catch (e) {
-    const { stderr } = e;
-
-    const matchParseErr = stderr.match(/, line ([0-9]+)/);
-    const matchCompErr = stderr.match(/([0-9]+):(.*)/);
-
-    // Relative line inside the codesnippet
-    let relLine;
-    let relPos;
-
-    if (matchParseErr) {
-      relLine = matchParseErr[1];
-    } else if (matchCompErr) {
-      relLine = matchCompErr[1];
-      relPos = matchCompErr[2];
+glob.sync(__dirname + '/../pages/**/*.mdx').forEach((file) => {
+  let content = fs.readFileSync(file, {encoding: 'utf-8'})
+  let parsedResult = parseFile(content)
+  if (parsedResult != null) {
+    fs.writeFileSync(tempFileName, parsedResult)
+    try {
+      child_process.execFileSync(bsc, ['-i', tempFileName], {stdio: 'pipe'})
+    } catch (e) {
+      process.stdout.write(postprocessOutput(file, e))
     }
-
-    // Markdown line + relative line - prelude lines = the actual error line
-    const errorLine = line + (relLine ? parseInt(relLine) : 0) - preludeLines;
-
-    // If there are parsing errors, there might not be a relPos (e.g. [line]:1-5)
-    const lineAndPos = errorLine + (relPos ? ":" + relPos : "");
-
-    const improvedStderr = stderr.replace(
-      /\/var.*/g,
-      `${relFilepath}: ${lineAndPos}`
-    );
-
-    //console.error(improvedStderr);
-    return { status: "failed", stderr: improvedStderr, line: errorLine };
   }
-};
+})
 
-const testPrelude = async (relFilepath, prelude) => {
-  const { line, value } = prelude;
-
-  const tempFile = tmp.fileSync();
-  const fileToFormat = tempFile.name + '.res';
-  fs.writeFileSync(fileToFormat, value);
-
-  console.log(`Testing prelude in '${relFilepath}' on line ${line}...`);
-
-  try {
-    const ret = await execFile(BSC, ["-i", fileToFormat]);
-    return { status: "ok" };
-  } catch (e) {
-    const { stderr } = e;
-
-    // Relative line inside the codesnippet
-    const [_, relLine, relPos] = stderr.match(/([0-9]+):(.*)/);
-
-    // Markdown line + relative line = the actual error line
-    const errorLine = line + (relLine ? parseInt(relLine) : 0);
-
-    // If there are parsing errors, there might not be a relPos (e.g. [line]:1-5)
-    const lineAndPos = errorLine + (relPos ? ":" + relPos : "");
-
-    const improvedStderr = stderr.replace(
-      /\/var.*/g,
-      `${relFilepath}: ${lineAndPos}`
-    );
-
-    //console.error(improvedStderr);
-    return { status: "failed", stderr: improvedStderr, line: errorLine };
-  }
-};
-
-const testFile = async filepath => {
-  const content = fs.readFileSync(filepath, "utf8");
-  const root = path.resolve(".");
-  const relFilepath = path.relative(root, filepath);
-
-  const result = processor.processSync(content);
-  const { examples, preludes } = result.data.candidates;
-
-  const testedPreludes = await Promise.all(
-    preludes.map(async prelude => {
-      const result = await testPrelude(relFilepath, prelude);
-      return {
-        ...prelude,
-        filepath: relFilepath,
-        result
-      };
-    })
-  );
-
-  const preludeFailed =
-    testedPreludes.filter(e => e.result.status === "failed").length > 0;
-
-  // Don't attempt to test the examples if one of the preludes is broken
-  if (preludeFailed) {
-    return testedPreludes;
-  }
-
-  // This is the prelude code which will get appended to each example
-  const fullPrelude = preludes.map(prelude => prelude.value).join("\n");
-
-  const testedExamples = await Promise.all(
-    examples.map(async example => {
-      const result = await testExample(relFilepath, example, fullPrelude);
-      return {
-        ...example,
-        filepath: relFilepath,
-        result
-      };
-    })
-  );
-
-  if (testedExamples.length > 0) {
-    console.log(`\n-------Results for '${relFilepath}'----------`);
-    testedExamples.forEach(e => {
-      const { line, filepath } = e;
-      const { status } = e.result;
-      console.log(`${filepath}:${line} => ${status}`);
-    });
-  }
-
-  return testedExamples;
-};
-
-const showErrorMsg = failedExample => {
-  const { stderr } = failedExample.result;
-  console.log(`\n-----------\nError Preview:`);
-  console.log(stderr);
-};
-
-const main = async () => {
-  const [, , pattern] = process.argv;
-  const files = glob.sync(pattern ? pattern : `${MD_DIR}/**/*.md?(x)`);
-
-  const allTested = await files.reduce(async (acc, filepath) => {
-    const tested = await testFile(filepath);
-    return [...(await acc), ...tested];
-  }, []);
-
-  const failed = allTested.filter(e => e.result.status === "failed");
-  const success = allTested.filter(e => e.result.status === "ok");
-
-  console.log("-----------\nSummary:");
-  console.log(`Total Examples: ${allTested.length}`);
-  console.log(`Failed: ${failed.length}`);
-  console.log(`Success: ${success.length}`);
-
-  if (failed.length > 0) {
-    console.log(
-      `\nTip: You can also run tests just for specific files / globs:`
-    );
-    console.log('`node scripts/test-examples.js pages/apis/latest/belt/array.mdx`');
-    showErrorMsg(failed[0]);
-    process.exit(1);
-  }
-};
-
-main();
+fs.unlinkSync(tempFileName)
