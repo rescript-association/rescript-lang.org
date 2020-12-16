12cca7e5cadd4c056c057ac32e8bf93d91b677a1
Patrick Stapfer <ryyppy@users.noreply.github.com>
2020-12-16 19:21:05 +0100

    Fix import path

--- pages/packages.js
+++ pages/packages.js
@@ -1,6 +1,6 @@
 import Comp from "re_pages/Packages"
 
-export { getStaticProps } from "re_pages/packages"
+export { getStaticProps } from "re_pages/Packages"
 
 export default function PackagesPage(props) {
   return <Comp {...props}/>;
