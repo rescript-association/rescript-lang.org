451bc44e83b44e286dd0bc28689302cbec2dee50
Tom Sherman <the.tomsherman@gmail.com>
2021-07-15 22:34:27 +0100

    Temporary next config workaround
    See https://github.com/vercel/next.js/issues/22581#issuecomment-864476385

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 const { ProvidePlugin } = require('webpack');
-const { ESBuildMinifyPlugin } = require('esbuild-loader');
 
 const bsconfig = require("./bsconfig.json");
-const path = require("path");
+const SSRPlugin = require("next/dist/build/webpack/plugins/nextjs-ssr-import")
+  .default;
+const { dirname, relative, resolve, join } = require("path");
 const remarkSlug = require("remark-slug");
 const fs = require("fs");
 
@@ -17,17 +18,42 @@ const withMdx = require("./plugins/next-mdx")({
 });
 
 
-// esbuild-loader specific features
-// See: https://github.com/privatenumber/esbuild-loader-examples/blob/master/examples/next/next.config.js
-function useEsbuildMinify(config, options) {
-  const terserIndex = config.optimization.minimizer.findIndex(minimizer => (minimizer.constructor.name === 'TerserPlugin'));
-  if (terserIndex > -1) {
-    config.optimization.minimizer.splice(
-      terserIndex,
-      1,
-      new ESBuildMinifyPlugin(options),
-    );
-  }
+// Unfortunately there isn't an easy way to override the replacement function body, so we
+// have to just replace the whole plugin `apply` body.
+function patchSsrPlugin(plugin) {
+  plugin.apply = function apply(compiler) {
+    compiler.hooks.compilation.tap("NextJsSSRImport", compilation => {
+      compilation.mainTemplate.hooks.requireEnsure.tap(
+        "NextJsSSRImport",
+        (code, chunk) => {
+          // This is the block that fixes https://github.com/vercel/next.js/issues/22581
+          if (!chunk.name) {
+            return;
+          }
+
+          // Update to load chunks from our custom chunks directory
+          const outputPath = resolve("/");
+          const pagePath = join("/", dirname(chunk.name));
+          const relativePathToBaseDir = relative(pagePath, outputPath);
+          // Make sure even in windows, the path looks like in unix
+          // Node.js require system will convert it accordingly
+          const relativePathToBaseDirNormalized = relativePathToBaseDir.replace(
+            /\\/g,
+            "/"
+          );
+          return code
+            .replace(
+              'require("./"',
+              `require("${relativePathToBaseDirNormalized}/"`
+            )
+            .replace(
+              "readFile(join(__dirname",
+              `readFile(join(__dirname, "${relativePathToBaseDirNormalized}"`
+            );
+        }
+      );
+    });
+  };
 }
 
 const isWebpack5 = true;
@@ -61,6 +87,14 @@ const config = {
         }
       });
       config.plugins.push(new ProvidePlugin({ React: "react" }));
+    }
+
+    const ssrPlugin = config.plugins.find(
+      plugin => plugin instanceof SSRPlugin
+    );
+
+    if (ssrPlugin) {
+      patchSsrPlugin(ssrPlugin);
     }
     return config;
   },
