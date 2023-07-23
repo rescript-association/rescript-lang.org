303fdf895fb2854f5616c389f15886044a50f59b
Pedro Castro <aspeddro@gmail.com>
2023-07-23 16:25:14 -0300

    testing doc gen

---
+++ scripts/doc-gen.mjs
@@ -0,0 +1,48 @@
+import fs from "fs";
+import path from "path";
+import child_process from "child_process";
+
+const COMPILER_PATH = "/home/pedro/Desktop/projects/rescript-compiler";
+const LIB_PATH = path.join(COMPILER_PATH, "lib", "ocaml");
+const ANALYSIS_PATH =
+  "/home/pedro/Desktop/projects/rescript-vscode/analysis/rescript-editor-analysis.exe";
+
+const all_files = fs.readdirSync(LIB_PATH);
+const files = all_files.filter(
+  file =>
+    file.endsWith(".res") ||
+    file.endsWith(".resi") ||
+    file.endsWith(".ml") ||
+    file.endsWith(".mli")
+);
+
+const interfaces = files.reduce((acc, file) => {
+  const interface_file = file + "i";
+  const is_interface = file.endsWith("i");
+  const interface_exists = files.includes(interface_file);
+
+  if (is_interface) {
+    return acc.includes(file) ? acc : [...acc, file];
+  } else {
+    return [...acc, interface_exists ? interface_file : file];
+  }
+}, []);
+
+const docs = interfaces.map(file => {
+  const full_path = path.join(LIB_PATH, file);
+  const process = child_process.execSync(
+    `${ANALYSIS_PATH} extractDocs ${full_path}`
+  );
+  return JSON.parse(process.toString());
+});
+
+const output_dir = path.join("index_data", "api");
+
+if (!fs.existsSync(output_dir)) {
+  fs.mkdirSync(output_dir);
+}
+
+docs.forEach(doc => {
+  const output_file = path.join(output_dir, doc.name);
+  fs.writeFileSync(`${output_file}.json`, JSON.stringify(doc, null, 2));
+});
