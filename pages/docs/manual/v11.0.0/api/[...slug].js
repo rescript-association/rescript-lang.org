54738929d4987c79f667033ee603933454359d57
Florian Hammerschmidt <fh@cca.io>
2024-12-19 22:16:36 +0100

    Update manual page links to versioned ones

--- pages/docs/manual/v11.0.0/api/[...slug].js
+++ pages/docs/manual/v11.0.0/api/[...slug].js
@@ -6,11 +6,11 @@ import {
 import APIDocs from "src/ApiDocs.mjs";
 
 export async function getStaticProps(ctx) {
-  return await getStaticPropsByVersion({ ...ctx, version: "latest" });
+  return await getStaticPropsByVersion({ ...ctx, version: "v11.0.0" });
 }
 
 export async function getStaticPaths(ctx) {
-  return await getStaticPathsByVersion("latest");
+  return await getStaticPathsByVersion("v11.0.0");
 }
 
 export default function Comp(props) {
